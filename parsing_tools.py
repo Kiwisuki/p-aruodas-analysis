01b1f9575ed035539c0c53a2cad91727c00d1f56
Marius Arlauskas <99347853+Kiwisuki@users.noreply.github.com>
2023-04-03 09:23:43 +0300

    structuring

---
+++ parsing_tools.py
@@ -0,0 +1,125 @@
+import undetected_chromedriver as uc
+import re
+from datetime import datetime
+from typing import List, Dict, Any, Tuple
+from lxml import html
+
+
+def while_replace(string: str) -> str:
+    """
+    Replace multiple consecutive spaces with a single space.
+    """
+    while '  ' in string:
+        string = string.replace('  ', ' ')
+    return string
+
+def text_strip_list(element_list: List[html.HtmlElement]) -> List[str]:
+    """Returns a list of text content stripped from the given list of HTML elements."""
+    return [element.text_content().strip() for element in element_list]
+
+
+
+def extract_element(tree: html.HtmlElement, class_name: str, index: int = 0) -> str:
+    """
+    Extracts the text content of an HTML element with the given class name.
+    Returns an empty string if the element does not exist.
+    """
+    elements = tree.body.find_class(class_name)
+    if elements:
+        return elements[index].text.strip()
+    else:
+        return ""
+    
+
+def extract_table(tree: html.HtmlElement) -> Dict[str, str]:
+    """
+    Extracts the table information from the HTML tree.
+    Returns a dictionary containing the table information.
+    """
+    table_dict = {}
+    table_elements = tree.body.find_class('obj-details')[0].findall('dd')
+    table_names = text_strip_list(tree.body.find_class('obj-details')[0].findall('dt'))
+    table_names = [name for name in table_names if name != '']
+    for i, name in enumerate(table_names):
+        table_dict[name] = table_elements[i].text.strip()
+    return table_dict
+
+
+def extract_thumbs(tree: html.HtmlElement) -> List[str]:
+    """Extracts and returns a list of URLs for the thumbnail images in the given HTML tree."""
+    thumbs = tree.find_class('link-obj-thumb')
+    thumbs = [thumb.get('href') for thumb in thumbs]
+    thumbs = set(thumbs)
+    thumbs.discard(None)
+    return thumbs
+
+
+def extract_photos(tree: html.HtmlElement) -> List[str]:
+    """
+    Extracts and returns a list of URLs for the photos in the given HTML tree.
+    Filters out URLs that do not contain 'img.dgn'.
+    """
+    urls = extract_thumbs(tree)
+    photos = []
+    for url in urls:
+        if 'img.dgn' in url:
+            photos.append(url)
+    return photos
+
+def extract_coordinates(tree: html.HtmlElement) -> Tuple[float, float]:
+    '''
+    Extracts and returns the coordinates of the property from the given HTML tree.
+    '''
+    urls = extract_thumbs(tree)
+    for url in urls:
+        if 'maps.google' in url:
+            return eval(url.split('=')[2])
+    return None
+
+def extract_number(tree: html.HtmlElement) -> Tuple[str, bool]:
+    """
+    Extracts the phone number and broker status from the HTML tree.
+    """
+    try:
+        phone = extract_element(tree, 'phone_item_0')
+        broker = True
+    except Exception as e:
+        print(e)
+        phone = extract_element(tree, 'phone')
+        broker = False
+    return phone, broker
+
+
+def extract_address(tree: html.HtmlElement) -> str:
+    """
+    Extracts the address of the property from the HTML tree.
+    """
+    address = tree.find_class('obj-header-text')[0].text_content().strip()
+    address = re.split(', \d+ kamb', address)[0]
+    return address
+
+
+def extract_table(tree: html.HtmlElement) -> Dict[str, str]:
+    """
+    Extract the table information from the HTML tree.
+    Returns a dictionary containing the table information.
+    """
+    table_dict = {}
+    table_elements = tree.body.find_class('obj-details')[0].findall('dd')
+    table_names = text_strip_list(tree.body.find_class('obj-details')[0].findall('dt'))
+    table_names = [name for name in table_names if name != '']
+    for i, name in enumerate(table_names):
+        table_dict[name] = table_elements[i].text.strip()
+    return table_dict
+
+def extract_ad_stats(tree: html.HtmlElement) -> Dict[str, str]:
+    """
+    Extract the ad stats information from the HTML tree.
+    Returns a dictionary containing the ad stats information.
+    """
+    ad_stats_dict = {}
+    ad_stats_names = text_strip_list(tree.find_class('obj-stats simple')[0].find('dl').findall('dt'))
+    ad_stats_values = text_strip_list(tree.find_class('obj-stats simple')[0].find('dl').findall('dd'))
+    for i, name in enumerate(ad_stats_names):
+        ad_stats_dict[name] = ad_stats_values[i]
+    return ad_stats_dict
---
+++ scraping_tools.py
@@ -0,0 +1,59 @@
+from typing import Tuple, Dict, Any, List
+from datetime import datetime
+import re
+import undetected_chromedriver as uc
+from lxml import html
+from parsing_tools import extract_element, text_strip_list, extract_photos, extract_table, extract_ad_stats, extract_address, extract_number, extract_coordinates
+
+
+def get_html(url: str) -> str:
+    """Returns the HTML content of the webpage with the given URL using headless Chrome."""
+    options = uc.ChromeOptions()
+    options.add_argument('--headless')
+    with uc.Chrome(options=options) as driver:
+        driver.get(url)
+        html_content = driver.page_source
+    return html_content
+
+def parse_property(url: str) -> Dict[str, Any]:
+    """
+    Extracts information about a property from a property listing page on aruodas.lt.
+
+    Args:
+        url (str): The url of the property listing page.
+
+    Returns:
+        Dict[str, Any]: A dictionary of the property information.
+    """
+    source = get_html(url)
+    tree = html.fromstring(source)
+    property_info = {
+        'Price': extract_element(tree, 'price-eur'),
+        'Address': extract_address(tree),
+        'Phone': extract_number(tree)[0],
+        'Broker': extract_number(tree)[1],
+        'Coordinates': extract_coordinates(tree),
+        'Reserved': extract_element(tree, 'reservation-strip__text'),
+        'Date_scraped': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        'Description': extract_element(tree, 'collapsedText'),
+        'Misc': text_strip_list(tree.body.find_class('special-comma')),
+        'Photos': extract_photos(tree)
+    }
+
+    ad_table = extract_table(tree)
+    ad_stats = extract_ad_stats(tree)
+
+    property_info.update(ad_table)
+    property_info.update(ad_stats)
+
+    property_info.pop('Ypatybės:', None)
+    property_info.pop('Papildomos patalpos:', None)
+    property_info.pop('Papildoma įranga:', None)
+    property_info.pop('Apsauga:', None)
+    return property_info
+
+
+if __name__ == '__main__':
+    url = 'https://www.aruodas.lt/butu-nuoma-vilniuje-salininkuose-mechaniku-g-tvarkingas-butas-puikiai-gyventi-ar-4-1224363/'
+    property_info = parse_property(url)
+    print(property_info)
